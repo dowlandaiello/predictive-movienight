c740ff2c3c9f714ce3357b3a3c1e3fb4c7333738
Dowland Aiello <dowlandaiello@gmail.com>
2021-08-06 22:32:08 -0400

    Update ratings
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- engine.R
+++ engine.R
@@ -71,10 +71,7 @@ mode <- function(x) {
   u <- unique(x)
   
   # Get the two most frequent values
-  paste(
-    u[sort(tabulate(match(x, u)), decreasing = TRUE)[1:2]],
-    collapse = "|"
-  )
+  paste(rownames(sort(table(x), decreasing=TRUE))[1:2], collapse="|")
 }
 
 # Collect the fam's movie ratings
@@ -85,6 +82,238 @@ watched <- rbind(
   ),
   rateTitle(
     "The Imitation Game",
+    c(9.0, 9.0, 9.0, 9.0)
+  ),
+  rateTitle(
+    "Three Billboards Outside Ebbing, Missouri",
+    c(8.0, 8.0, 8.5, 10.0)
+  ),
+  rateTitle(
+    "Adaptation",
+    c(7.0, 8.0, 8.0, 10.0)
+  ),
+  rateTitle(
+    "Nebraska",
+    c(8.0, 9.0, 8.5, 9.0)
+  ),
+  rateTitle(
+    "The Lady in the Van",
+    c(9.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Four Lions",
+    c(6.0, 8.5, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Baby Driver",
+    c(8.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Safety Not Guaranteed",
+    c(7.0, 8.0, 8.0, 8.5)
+  ),
+  rateTitle(
+    "Amélie",
+    c(8.0, 8.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Up in the Air",
+    c(6.0, 8.0, 7.0, 7.5)
+  ),
+  rateTitle(
+    "Cinema Paradiso",
+    c(8.0, 8.0, 8.5, 9.0)
+  ),
+  rateTitle(
+    "The Big Lebowski",
+    c(9.0, 9.0, 9.0, 9.0)
+  ),
+  rateTitle(
+    "Spy",
+    c(8.0, 9.0, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Shaun of the Dead",
+    c(9.0, 9.0, 9.5, 9.0)
+  ),
+  rateTitle(
+    "Hot Fuzz",
+    c(8.0, 8.5, 8.5, 9.0)
+  ),
+  rateTitle(
+    "The World's End",
+    c(5.0, 8.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Mission: Impossible - Fallout",
+    c(7.0, 9.0, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Mission: Impossible - Rogue Nation",
+    c(7.0, 9.0, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Mission: Impossible - Ghost Protocol",
+    c(7.0, 9.0, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Game Night",
+    c(7.0, 8.5, 8.5, 8.5)
+  ),
+  rateTitle(
+    "The Shawshank Redemption",
+    c(8.0, 9.0, 9.5, 9.5)
+  ),
+  rateTitle(
+    "Chef",
+    c(9.0, 9.0, 8.0, 9.0)
+  ),
+  rateTitle(
+    "Starsky & Hutch",
+    c(8.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Borat: Cultural Learnings of America for Make Benefit Glorious Nation of Kazakhstan",
+    c(6.0, 9.0, 9.0, 9.5)
+  ),
+  rateTitle(
+    "Borat Subsequent Moviefilm",
+    c(6.0, 9.0, 9.0, 9.5)
+  ),
+  rateTitle(
+    "What We Do in the Shadows",
+    c(6.0, 8.0, 8.0, 8.5)
+  ),
+  rateTitle(
+    "The Big Sick",
+    c(7.0, 9.0, 9.0, 8.5)
+  ),
+  rateTitle(
+    "Raising Arizona",
+    c(7.0, 7.0, 7.5, 8.0)
+  ),
+  rateTitle(
+    "The Nice Guys",
+    c(7.0, 8.0, 8.0, 8.5)
+  ),
+  rateTitle(
+    "Pineapple Express",
+    c(7.0, 8.0, 8.0, 6.5)
+  ),
+  rateTitle(
+    "Best in Show",
+    c(7.0, 8.5, 8.0, 9.0)
+  ),
+  rateTitle(
+    "Fantastic Mr. Fox",
+    c(8.0, 8.0, 8.0, 9.5)
+  ),
+  rateTitle(
+    "Fargo",
+    c(7.0, 8.0, 8.0, 8.5)
+  ),
+  rateTitle(
+    "Midnight in Paris",
+    c(9.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Meet the Parents",
+    c(8.0, 8.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "Django Unchained",
+    c(8.0, 8.0, 9.5, 9.5)
+  ),
+  rateTitle(
+    "Superbad",
+    c(9.0, 10.0, 9.5, 9.5)
+  ),
+  rateTitle(
+    "The Sapphires",
+    c(9.0, 9.0, 8.0, 8.5)
+  ),
+  rateTitle(
+    "The Godfather",
+    c(6.0, 7.0, 7.5, 10.0)
+  ),
+  rateTitle(
+    "Rango",
+    c(9.0, 9.0, 8.0, 9.0)
+  ),
+  rateTitle(
+    "Goodfellas",
+    c(7.0, 7.0, 8.0, 9.0)
+  ),
+  rateTitle(
+    "The Score",
+    c(8.0, 8.0, 7.5, 7.0)
+  ),
+  rateTitle(
+    "In Bruges",
+    c(9.0, 9.0, 8.0, 9.0)
+  ),
+  rateTitle(
+    "The Martian",
+    c(7.0, 9.0, 8.5, 8.0)
+  ),
+  rateTitle(
+    "Interstellar",
+    c(6.0, 7.0, 7.5, 9.0)
+  ),
+  rateTitle(
+    "Robot & Frank",
+    c(9.0, 9.0, 8.5, 9.0)
+  ),
+  rateTitle(
+    "Mars Attacks!",
+    c(7.0, 8.0, 8.0, 6.0)
+  ),
+  rateTitle(
+    "Bandits",
+    c(9.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "The Social Network",
+    c(7.0, 7.0, 7.5, 8.0)
+  ),
+  rateTitle(
+    "Paddleton",
+    c(9.0, 9.0, 8.5, 9.0)
+  ),
+  rateTitle(
+    "Paddington 2",
+    c(9.0, 9.0, 9.5, 9.5)
+  ),
+  rateTitle(
+    "Jojo Rabbit",
+    c(9.5, 9.5, 9.0, 9.5)
+  ),
+  rateTitle(
+    "Hunt for the Wilderpeople",
+    c(8.0, 8.5, 8.5, 8.5)
+  ),
+  rateTitle(
+    "Hearts Beat Loud",
+    c(9.0, 9.0, 7.5, 8.0)
+  ),
+  rateTitle(
+    "Music and Lyrics",
+    c(7.5, 7.5, 6.5, 6.5)
+  ),
+  rateTitle(
+    "Sing Street",
+    c(9.0, 9.0, 8.5, 9.0)
+  ),
+  rateTitle(
+    "Once",
+    c(9.0, 9.0, 8.0, 8.0)
+  ),
+  rateTitle(
+    "City Island",
+    c(9.0, 9.5, 9.0, 9.5)
+  ),
+  rateTitle(
+    "O Brother, Where Art Thou?",
     c(9.0, 9.0, 9.0, 9.0)
   )
 )
@@ -134,8 +363,11 @@ context <- ratedTitles %>%
     score = (
       tasteRelevance + audienceFactor
     )
-  ) %>%
-  arrange(desc(score))
+  )
+context[grep("Comedy", genres)]$score =
+  context[grep("Comedy", genres)]$score * 1.5
+
+context <- context %>% arrange(desc(score))
 
 # Differentiate between our and their ratings so we can highlight them on the
 # chart
@@ -147,9 +379,9 @@ bestMovies <- context %>%
 # Graph the distribution of movies that appeal to our tastes!
 summaryBestMovies <- bestMovies[bestMovies %>% nrow %>% sample(50)] %>%
   plot_ly(x = ~tasteRelevance, y = ~audienceFactor, z = ~score,
-          type = "scatter3d", mode = "markers")
-  #add_markers(x = ~ourRatings$tasteRelevance, y = ~ourRatings$audienceFactor,
-              #z = ~ourRatings$score)
+          type = "scatter3d", mode = "markers") %>%
+  add_markers(x = ~ourRatings$tasteRelevance, y = ~ourRatings$audienceFactor,
+              z = ~ourRatings$score)
 summaryBestMovies 
 
-bestMovies %>% head(200) %>% write.csv(file = "res")
+# bestMovies %>% head(200) %>% write.csv(file = "res")
